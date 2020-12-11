9781fd368ae5512f7e75f20b14ab937d5858490c
José Carlos Duque Filho <cj_duque@hotmail.com>
2020-12-11 12:03:25 -0300

    commit

---
+++ Test/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Entities {
+    class Couser {
+        public int Number { get; set; }
+
+        public Couser(int number) {
+            Number = number;
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Couser)) {
+                return false;
+            }
+            Couser other = obj as Couser;
+            return Number.Equals(other.Number);
+        }
+
+        public override int GetHashCode() {
+            return Number.GetHashCode();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Test.Entities;
+
+
+namespace Test {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("How many students for couser: ");
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<Couser> couserA = new HashSet<Couser>();
+            HashSet<Couser> couserB = new HashSet<Couser>();
+            HashSet<Couser> couserC = new HashSet<Couser>();
+
+            for (int i = 1; i <= n; i++) {
+                int student = int.Parse(Console.ReadLine());
+                couserA.Add(new Couser(student));
+            }
+
+            Console.Write("How many students for couser: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                int student = int.Parse(Console.ReadLine());
+                couserB.Add(new Couser(student));
+            }
+
+            Console.Write("How many students for couser: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                int student = int.Parse(Console.ReadLine());
+                couserC.Add(new Couser(student));
+            }
+
+            HashSet<Couser> total = new HashSet<Couser>(couserA);
+            total.UnionWith(couserB);
+            
+            total.UnionWith(couserC);
+            Console.WriteLine("Total students: " + total.Count);
+        }
+    }
+}
